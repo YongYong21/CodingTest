da467862c0bfd3dcf4b29357d6bd7b2a13c17d7f
박용희 <90038848+YongYong21@users.noreply.github.com>
2023-08-02 11:30:49 +0900

    [level 1] Title: 음양 더하기, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -0,0 +1,8 @@
+function solution(absolutes, signs) {
+    for(let i=0 ; i < absolutes.length; i++){
+        if(signs[i]==false){
+            absolutes[i] = -absolutes[i]
+        }
+    }
+    return absolutes.reduce((a,b)=> a+b,0)
+}
