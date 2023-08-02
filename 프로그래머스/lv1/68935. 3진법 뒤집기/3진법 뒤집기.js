096600cdea6018804924156c05f5409033b63cfb
박용희 <90038848+YongYong21@users.noreply.github.com>
2023-08-02 11:35:42 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -0,0 +1,8 @@
+function solution(n) {
+    return parseInt(n
+                    .toString(3)
+                    .split('')
+                    .reverse()
+                    .join('')
+                    , 3)
+}
