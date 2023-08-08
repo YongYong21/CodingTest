24a26085ea7e4e8f91293e095c23d828cfda289d
박용희 <90038848+YongYong21@users.noreply.github.com>
2023-08-08 15:47:24 +0900

    [unrated] Title: 콜라 문제, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
@@ -1,6 +1,5 @@
 function solution(a, b, n) {
     let coke = 0;
-    let empty = 0;
     while(n >= a){
         coke += Math.floor(n/a) * b
         n = n - a * Math.floor(n/a) + Math.floor(n/a) * b
