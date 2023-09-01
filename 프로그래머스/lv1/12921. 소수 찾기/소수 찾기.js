b190ee2bf1acbcd8a08144a772400b84d51a82ab
박용희 <90038848+YongYong21@users.noreply.github.com>
2023-09-02 00:18:55 +0900

    [level 1] Title: 소수 찾기, Time: 21.53 ms, Memory: 45.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -2,24 +2,18 @@ function solution(n) {
     const isPrime = Array(n + 1).fill(true); // 모든 수를 소수로 초기화
     isPrime[0] = isPrime[1] = false; // 0과 1은 소수가 아님
 
+    const primes = [];
 
-    for (let i = 2; i * i <= n; i++) {
+    for (let i = 2; i <= n; i++) {
         if (isPrime[i]) {
-            for (let j = i * i; j <= n; j += i) {
-                isPrime[j] = false;
+            primes.push(i); // 소수를 저장
+            for (let j = i * 2; j <= n; j += i) {
+                isPrime[j] = false; // i의 배수들을 모두 소수가 아닌 것으로 표시
             }
         }
     }
 
-
-    let count = 0;
-    for (let i = 2; i <= n; i++) {
-        if (isPrime[i]) {
-            count++;
-        }
-    }
-
-    return count;
+    return primes.length;
 }
 
 
